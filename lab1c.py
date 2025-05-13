3beba1cf7657315dacd3baa44638de1f3a8158d6
Agnestra Mahat <amahat1@myseneca.ca>
2025-05-13 15:47:27 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#The name and age of Isaac are as follows:
+
+name = 'Isaac'
+age = int(72)
+
+#This prints out issacs age. I googled for f and {} as i did not find it on the course wiki while doing lab1b
+
+print(f"{name} is {age} years old!")
