db9b4dc0f08ee6c15414552b0b8138e63af592dc
Agnestra Mahat <amahat1@myseneca.ca>
2025-05-16 15:35:25 -0400

    Added lad1d

---
+++ lab1d.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#These defines the values of x,y and z which when changed changes the print out values for the following print function.
+
+x = 10
+y = 2
+z = 5
+
+#This line does the maths the required output took a while to figure out that the x+y*z needs curly brackets.
+print(f"{x} + {y} * {z} = {x+y*z}")
